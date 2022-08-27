fb9a34028a4b3f24a1845b9aad26a2e3dc9cd0e7
Muhammad Ibrahim <ia4bwp@gmail.com>
2022-08-27 23:53:48 +0500

    Completed video#23

---
+++ src/Components/NewItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewItem extends Component {
+  render() {
+    return (
+      <div>This is a news item</div>
+    )
+  }
+}
+
+export default NewItem
